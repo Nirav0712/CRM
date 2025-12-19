fa8da7f0020743f9b22d0ca0611bc21c656b6ddf
mrmajithiya <oom863572@gmail.com>
2025-12-19 19:58:53 +0530

    fivth commit

--- src/app/api/tasks/[id]/notes/route.ts
+++ src/app/api/tasks/[id]/notes/route.ts
@@ -3,6 +3,8 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
 import { prisma } from "@/lib/prisma";
 
+export const dynamic = 'force-dynamic';
+
 // POST add note to task (admin can add notes/responses)
 export async function POST(
     request: NextRequest,
